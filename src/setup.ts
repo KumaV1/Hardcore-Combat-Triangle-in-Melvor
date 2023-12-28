9a201c8b1723ff4a30f8ca3c81998c9212175bfe
KumaV1 <KumaV1>
2023-12-28 22:07:51 +0100

    Added patch to forcefully return the hardcore combat triangle whenever it is accessed on the Player object

--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,8 @@
 import '../assets/Logo.png'
 
 export async function setup(ctx: Modding.ModContext) {
-    console.log("Test");
+    // @ts-ignore Typescript, for some reason, thinks this is a method patch, despite working without issue in the end
+    ctx.patch(Player, "activeTriangle").get(function (original) {
+        return combatTriangle[CombatTriangles.Hardcore];
+    });
 }
